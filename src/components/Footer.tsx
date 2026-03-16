21072f5ddbfba90073392b6d745abf3e795e3405
Kieran Hunt <hunkiera@amazon.com>
2026-03-16 13:50:13 +0000

    Add Raycast quicklink download to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,19 @@ import indexData from "../../public/search-index.json";
 import type { Element } from "../types";
 import { DotSwarm } from "./DotSwarm";
 import { GitHubLogo } from "./GitHubLogo";
+import { RaycastLogo } from "./RaycastLogo";
 
 const elements: Element[] = indexData.elements as Element[];
 const generatedAt: string = indexData.generatedAt as string;
+
+const quicklinkJson = JSON.stringify([
+	{
+		name: "cdk-search",
+		link: 'https://cdk-search.kieran.casa/?q={argument name="query"}',
+		iconName: "magnifying-glass-16",
+	},
+]);
+const quicklinkUrl = URL.createObjectURL(new Blob([quicklinkJson], { type: "application/json" }));
 
 export const Footer = () => (
 	<div className="mt-4 px-3 text-xs text-slate-400 grid grid-cols-2 gap-x-4">
@@ -23,6 +33,16 @@ export const Footer = () => (
 				className="no-underline hover:text-slate-300"
 			>
 				<GitHubLogo /> Github
+			</a>
+		</p>
+		<p />
+		<p className="text-right">
+			<a
+				href={quicklinkUrl}
+				download="cdk-search.quicklink.json"
+				className="no-underline hover:text-slate-300"
+			>
+				Add to <RaycastLogo /> Raycast
 			</a>
 		</p>
 	</div>
